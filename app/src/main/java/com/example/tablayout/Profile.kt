0950c187c26271311d166aeb552636927444f490
ST10088734 <st10088734@vcconnect.edu.za>
2024-09-13 13:24:09 +0200

    Settings page

--- app/src/main/java/com/example/tablayout/Profile.kt
+++ app/src/main/java/com/example/tablayout/Profile.kt
@@ -8,13 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.UserProfileChangeRequest
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
