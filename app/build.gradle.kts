4c2b1c4812ad4269a54a81f1e1660e3a2ea47b32
ST10088734 <st10088734@vcconnect.edu.za>
2024-10-30 21:48:04 +0200

    Update README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,10 +86,6 @@ dependencies {
     Available at: https://www.youtube.com/watch?v=_dCRQ9wta-I
     [Accessed 12 October 2024].*/
     implementation("androidx.biometric:biometric:1.1.0")
-
-    // Coroutines for Kotlin
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
 }
 
 
